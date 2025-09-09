87bd7180ca6f3b725d6888ee9a08918f9d407b9e
shivam56970 <84052845+shivam56970@users.noreply.github.com>
2025-09-09 06:54:55 +0530

    Update app.js
    dot env

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/../.env" });
 const express = require("express");
 const { connectDB } = require("./config/database");
 const { loginValidation } = require("./utils/validation");
